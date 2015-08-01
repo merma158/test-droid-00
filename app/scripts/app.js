031d08475d7f13cfc97ee8106512e5754fed6a62
Javier Alexander <javierurbano11@gmail.com>
2015-08-01 21:58:59 +0000

    login screen Changes made by Javier Alexander (javierurbano11@gmail.com).

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,6 +27,47 @@
     });
 
     function initialize() {
+        /*
+        el.Users.login("demo", "demo.1",
+          function(data) {
+              groceryDataSource.read();
+          }
+        );
+        */
+        window.loginView = kendo.observable({
+          submit: function() {
+              if (!this.username) {
+                  navigator.notification.alert("Username is required.");
+                  return;
+              }
+              if (!this.password) {
+                  navigator.notification.alert("Password is required.");
+                  return;
+              }
+              el.Users.login(this.username, this.password,
+                  function(data) {
+                      window.location.href = "#list";
+                      groceryDataSource.read();
+                  }, function() {
+                      navigator.notification.alert("Unfortunately we could not find your account.");
+                  });
+          }
+        });
+        
+        window.listView = kendo.observable({
+          logout: function(event) {
+              // Prevent going to the login page until the login call processes.
+              event.preventDefault();
+              el.Users.logout(function() {
+                  this.loginView.set("username", "" );
+                  this.loginView.set("password", "");
+                  window.location.href = "#login";
+              }, function() {
+                  navigator.notification.alert("Unfortunately an error occurred logging out of your account.");
+              });
+          }
+        });
+        
         window.addView = kendo.observable({
           add: function() {
               if (!this.grocery) {
