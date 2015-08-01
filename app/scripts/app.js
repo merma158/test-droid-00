f071c98e451fb61e9c5b4fe2e033ff226285894f
Javier Alexander <javierurbano11@gmail.com>
2015-08-01 21:02:02 +0000

    Get and Set data to backend Changes made by Javier Alexander (javierurbano11@gmail.com).

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,8 @@
 (function() {
-    var apiKey = "YOUR API KEY";
+    var apiKey = "SCB2d1hANjfQuknl";
     var el = new Everlive(apiKey);
-
+	
+    /*
     var groceryDataSource = new kendo.data.DataSource({
         transport: {
             read: {
@@ -15,13 +16,47 @@
             }
         }
     });
+    */
+    
+    var groceryDataSource = new kendo.data.DataSource({
+      type: "everlive",
+      sort: { field: "Name", dir: "asc" },
+      transport: {
+          typeName: "Groceries"
+      }
+    });
 
     function initialize() {
+        window.addView = kendo.observable({
+          add: function() {
+              if (!this.grocery) {
+                  navigator.notification.alert("Please provide a grocery.");
+                  return;
+              }
+
+              groceryDataSource.add({ Name: this.grocery });
+              groceryDataSource.one("sync", this.close);
+              groceryDataSource.sync();
+          },
+          close: function() {
+              $("#add").data("kendoMobileModalView").close();
+              this.grocery = "";
+          }
+        });
+        
         var app = new kendo.mobile.Application(document.body, {
             skin: "flat",
             transition: "slide"
         });
+        
         navigator.splashscreen.hide();
+        
+        $("#grocery-list").kendoMobileListView({
+          dataSource: groceryDataSource,
+          template: "#: Name #"
+        });
+        
+        
     }
 
     document.addEventListener("deviceready", initialize);
